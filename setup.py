251fb20ff33baae1817ccecb13595379255ab843
Greg Stupp <gstupp@scripps.edu>
2016-09-16 14:14:00 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="mydisease",
+    version="0.1",
+    packages=['mydisease','mydisease.dataload','mydisease.utils','mydisease.dataload.diseaseontology','mydisease.dataload.ctdbase']
+)
