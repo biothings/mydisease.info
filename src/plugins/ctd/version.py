7c85cb303d422b02fca99cda5410af95f4da7b67
jiwen xin <kevinxin@scripps.edu>
2020-10-26 10:50:22 -0700

    update ctd get version

--- src/plugins/ctd/version.py
+++ src/plugins/ctd/version.py
@@ -6,6 +6,6 @@ def get_release(self):
         last_modified = res.headers.get(
             "Last-Modified", "Thu, 01 Oct 2020 20:17:32 GMT"
         )
-        return last_modified.split(",")[-1].split(":")[0][:-2].strip()
+        return last_modified
     except:
         return "Thu, 01 Oct 2020 20:17:32 GMT"
