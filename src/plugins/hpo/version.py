bd98613181c32817370fada3b8ade5cf7536a503
jiwen xin <kevinxin@scripps.edu>
2020-10-26 10:42:12 -0700

    update hpo get version

--- src/plugins/hpo/version.py
+++ src/plugins/hpo/version.py
@@ -1,2 +1,11 @@
 def get_release(self):
-    return "2020.3"
+    import requests
+
+    doc = requests.get(
+        "http://compbio.charite.de/jenkins/job/hpo.annotations.current/lastSuccessfulBuild/artifact/current/phenotype.hpoa"
+    )
+    for line in doc.iter_lines():
+        line = line.decode("utf-8")
+        if line.startswith("#date:"):
+            version = line.split(":")[-1].strip()
+            return version
