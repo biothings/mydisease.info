76bfb0533ad1ee94a35593f52a476f1ae188c28d
jiwen xin <kevinxin@scripps.edu>
2020-10-26 10:10:27 -0700

    add disease ontology check version

--- src/plugins/disease_ontology/version.py
+++ src/plugins/disease_ontology/version.py
@@ -1,2 +1,11 @@
 def get_release(self):
-    return "2020-08"
+    import requests
+
+    doc = requests.get(
+        "https://raw.githubusercontent.com/DiseaseOntology/HumanDiseaseOntology/main/src/ontology/doid.obo"
+    )
+    for line in doc.iter_lines():
+        line = line.decode("utf-8")
+        if line.startswith("data-version:"):
+            version = line.split(":")[-1].split("/")[-2]
+            return version
