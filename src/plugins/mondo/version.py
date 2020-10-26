0c0cbf1dc0e52e51c523486af2cf4d105bad174b
jiwen xin <kevinxin@scripps.edu>
2020-10-26 10:49:40 -0700

    update mondo get version

--- src/plugins/mondo/version.py
+++ src/plugins/mondo/version.py
@@ -4,6 +4,6 @@ def get_release(self):
     doc = requests.get("http://purl.obolibrary.org/obo/mondo.obo")
     for line in doc.iter_lines():
         line = line.decode("utf-8")
-        if line.startswith("#date:"):
+        if line.startswith("data-version:"):
             version = line.split(":")[-1].strip()
             return version
