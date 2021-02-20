dce2b55501aa63373b04168e5ed446ff546f292f
jiwen xin <kevinxin@scripps.edu>
2021-02-19 20:08:08 -0800

    update hpo version;

--- src/plugins/hpo/version.py
+++ src/plugins/hpo/version.py
@@ -2,10 +2,6 @@ def get_release(self):
     import requests
 
     doc = requests.get(
-        "http://compbio.charite.de/jenkins/job/hpo.annotations.current/lastSuccessfulBuild/artifact/current/phenotype.hpoa"
+        "http://purl.obolibrary.org/obo/hp/hpoa/phenotype.hpoa"
     )
-    for line in doc.iter_lines():
-        line = line.decode("utf-8")
-        if line.startswith("#date:"):
-            version = line.split(":")[-1].strip()
-            return version
+    return doc.get("headers", {}).get("last-modified");
