f59108a8e83cd53f18bcd2e542fb922a9d88137f
Zhongchao Qian <zcqian@users.noreply.github.com>
2021-04-30 16:18:46 -0700

    Add App Tests for ANNOTATION_DEFAULT_SCOPES (#34)
    * initial tests
    
    * add more tests
    
    * tests use real data
    
    * Create config.py
    
    * use value_in_result method

---
+++ src/tests/test_apptests.py
@@ -0,0 +1,38 @@
+import glob
+import json
+import os
+
+import pytest
+import elasticsearch
+from biothings.tests.web import BiothingsWebAppTest
+
+
+class TestMyDiseaseConfigDefaultScopes(BiothingsWebAppTest):
+    TEST_DATA_DIR_NAME = 'DefaultScopes'
+
+    def test_010_id(self):
+        q = 'MONDO:0010936'
+        res = self.request("disease", method="POST", data={"ids": q})
+        res = res.json()
+        assert len(res) == 1
+        assert res[0]['_id'].lower() == q.lower()
+
+    def test_011_doid(self):
+        q = 'doid:0111227'
+        res = self.request("disease", method="POST", data={"ids": q})
+        res = res.json()
+        assert len(res) == 1
+        assert self.value_in_result(q, res, 'disease_ontology.doid', True)
+
+    def test_012_mesh(self):
+        q = 'c579991'
+        res = self.request("disease", method="POST", data={"ids": q})
+        res = res.json()
+        assert len(res) == 1
+        assert self.value_in_result(q, res, 'mondo.xrefs.mesh', True)
+
+    def test_020_does_not_search_all(self):
+        q = 'DOID:0060208'  # mondo.xrefs.doid is copied to all
+        res = self.request("disease", method="POST", data={"ids": q})
+        res = res.json()
+        assert res[0]['notfound']
