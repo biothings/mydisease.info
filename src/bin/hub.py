271b0b01e9553eb6b1b7582ee94d8ac7b050eda6
Ricardo Avila <ravila@scripps.edu>
2022-08-11 10:05:01 -0700

    Simplify hub config

--- src/bin/hub.py
+++ src/bin/hub.py
@@ -6,9 +6,6 @@ from biothings.utils.version import set_versions
 
 app_folder,_src = os.path.split(os.path.split(os.path.split(os.path.abspath(__file__))[0])[0])
 set_versions(config, app_folder)
-biothings.config_for_app(config)
-logging = config.logger
-
 
 from biothings.hub import HubServer
 import hub.dataload.sources
@@ -17,6 +14,7 @@ server = HubServer(hub.dataload.sources, name=config.HUB_NAME)
 
 
 if __name__ == "__main__":
+    logging = config.logger
     logging.info("Hub DB backend: %s", biothings.config.HUB_DB_BACKEND)
     logging.info("Hub database: %s", biothings.config.DATA_HUB_DB_DATABASE)
     server.start()
