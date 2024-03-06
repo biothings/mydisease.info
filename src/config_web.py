4190e8fcc7d358a95a1fba978d8b0a58381c6eb8
jschaff <jschaff@scripps.edu>
2024-03-06 15:50:54 -0800

    Add tests for evaluating the mydisease curie support

--- src/config_web.py
+++ src/config_web.py
@@ -3,7 +3,7 @@ import re
 # *****************************************************************************
 # Elasticsearch variables
 # *****************************************************************************
-ES_HOST = "localhost:9200"
+ES_HOST = "es8.biothings.io:9200"
 ES_ARGS = {"timeout": 60}
 ES_INDICES = {"disease": "mydisease_current"}
 
@@ -18,22 +18,50 @@ API_VERSION = "v1"
 
 
 # CURIE ID support based on BioLink Model
-BIOLINK_MODEL_PREFIX_BIOTHINGS_GENE_MAPPING = {
-    "MONDO": {"type": "disease", "field": "mondo.mondo", "keep_prefix": True},
-    "DOID": {"type": "disease", "field": "disease_ontology.doid", "keep_prefix": True},
+BIOLINK_MODEL_PREFIX_BIOTHINGS_DISEASE_MAPPING = {
+    "MONDO": {
+        "type": "disease",
+        "field": ["mondo.mondo"],
+        "regex_term_pattern": "(?P<term>MONDO:[0-9]+)",
+    },
+    "DOID": {
+        "type": "disease",
+        "field": ["disease_ontology.doid"],
+        "regex_term_pattern": "(?P<term>DOID:[0-9]+)",
+    },
 }
 biolink_curie_regex_list = []
 for (
     biolink_prefix,
     mapping,
-) in BIOLINK_MODEL_PREFIX_BIOTHINGS_GENE_MAPPING.items():
-    expression = re.compile(rf"({biolink_prefix}):(?P<term>[^:]+)", re.I)
-    field_match = mapping["field"]
-    pattern = (expression, field_match)
+) in BIOLINK_MODEL_PREFIX_BIOTHINGS_DISEASE_MAPPING.items():
+    field_match = mapping.get("field", [])
+    term_pattern = mapping.get("regex_term_pattern", None)
+    if term_pattern is None:
+        term_pattern = "(?P<term>[^:]+)"
+
+    raw_expression = rf"({biolink_prefix}):{term_pattern}"
+    compiled_expression = re.compile(raw_expression, re.I)
+
+    pattern = (compiled_expression, field_match)
     biolink_curie_regex_list.append(pattern)
+
+# id regex pattern
+id_disease_regex_pattern = (re.compile(r"([\w]+):([0-9]+)", re.I), ["_id"])
+
+# The default for the disease requires more care due to the potential for miscellaneous
+# term values to include the ":" character. So we want to match for anything in the scope value
+# up to the first ":", which end that group matching and move on to the term group matching. We then
+# want to be sure we include the ":" in the value matching associated with the term for values like
+# DOID:####### or MONDO:#######
+default_disease_regex = re.compile(r"([^:]+):(?P<term>[\W\w]+)")
+default_disease_fields = ()
+default_disease_regex_pattern = (default_disease_regex, default_disease_fields)
 
 ANNOTATION_ID_REGEX_LIST = [
     *biolink_curie_regex_list,
+    id_disease_regex_pattern,
+    default_disease_regex_pattern,
 ]
 
 ANNOTATION_DEFAULT_SCOPES = ["_id", "disease_ontology.doid", "mondo.xrefs.mesh"]
