6a4479a2959beacd7e7abc70e91f0cb793505a4a
Dylan Welzel <dylanwelzel@gmail.com>
2024-04-25 13:12:53 -0700

    standardized version dates

--- src/plugins/ctd/version.py
+++ src/plugins/ctd/version.py
@@ -1,11 +1,16 @@
 def get_release(self):
+    import datetime
+
     import requests
 
-    res = requests.get("http://ctdbase.org/reports/CTD_chemicals_diseases.csv.gz")
+    res = requests.get(
+        "http://ctdbase.org/reports/CTD_chemicals_diseases.csv.gz")
     try:
         last_modified = res.headers.get(
             "Last-Modified", "Thu, 01 Oct 2020 20:17:32 GMT"
         )
-        return last_modified
+        return datetime.datetime.strptime(
+            last_modified, "%a, %d %b %Y %H:%M:%S %Z"
+        ).strftime("%Y-%m-%d")
     except:
         return "Thu, 01 Oct 2020 20:17:32 GMT"
