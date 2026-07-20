6fa47c706eae0b34709f0d0e2e43e6aad0638ace
Dylan Welzel <dylanwelzel@gmail.com>
2026-07-20 12:15:54 -0700

    test canonical cross-batch merge

---
+++ src/tests/test_builder.py
@@ -0,0 +1,102 @@
+import sys
+import types
+
+
+biothings = types.ModuleType("biothings")
+hub_mod = types.ModuleType("biothings.hub")
+databuild_mod = types.ModuleType("biothings.hub.databuild")
+builder_dep = types.ModuleType("biothings.hub.databuild.builder")
+mapper_dep = types.ModuleType("biothings.hub.databuild.mapper")
+utils_mod = types.ModuleType("biothings.utils")
+mongo_mod = types.ModuleType("biothings.utils.mongo")
+pymongo_mod = types.ModuleType("pymongo")
+
+
+class DataBuilder:
+    pass
+
+
+class IDBaseMapper:
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class ReplaceOne:
+    pass
+
+
+builder_dep.DataBuilder = DataBuilder
+mapper_dep.IDBaseMapper = IDBaseMapper
+mongo_mod.doc_feeder = lambda *args, **kwargs: []
+mongo_mod.get_target_db = lambda: None
+mongo_mod.get_src_db = lambda: None
+pymongo_mod.ReplaceOne = ReplaceOne
+
+sys.modules.update({
+    "biothings": biothings,
+    "biothings.hub": hub_mod,
+    "biothings.hub.databuild": databuild_mod,
+    "biothings.hub.databuild.builder": builder_dep,
+    "biothings.hub.databuild.mapper": mapper_dep,
+    "biothings.utils": utils_mod,
+    "biothings.utils.mongo": mongo_mod,
+    "pymongo": pymongo_mod,
+})
+
+from hub.databuild import builder as builder_module
+from hub.databuild.builder import CanonicalDataBuilder
+
+
+class FakeReplaceOne:
+    def __init__(self, filter_, replacement, upsert=False):
+        self.filter = filter_
+        self.replacement = replacement
+        self.upsert = upsert
+
+
+class FakeCollection:
+    def __init__(self, docs):
+        self.docs = docs
+        self.ops = []
+
+    def find(self, query):
+        ids = query["_id"]["$in"]
+        return [self.docs[_id] for _id in ids if _id in self.docs]
+
+    def bulk_write(self, ops):
+        self.ops.extend(ops)
+        for op in ops:
+            self.docs[op.filter["_id"]] = op.replacement
+
+
+def test_upsert_merged_docs_merges_canonical_docs_across_batches(monkeypatch):
+    monkeypatch.setattr(builder_module, "ReplaceOne", FakeReplaceOne)
+    builder = CanonicalDataBuilder.__new__(CanonicalDataBuilder)
+    temp_col = FakeCollection({
+        "MONDO:0005072": {
+            "_id": "MONDO:0005072",
+            "mondo": {
+                "mondo": "MONDO:0005072",
+                "label": "neuroblastoma",
+            },
+        }
+    })
+    merged_docs = {
+        "MONDO:0005072": {
+            "_id": "MONDO:0005072",
+            "original_id": "UMLS:C0700095",
+            "umls": {
+                "umls": "C0700095",
+            },
+        }
+    }
+
+    builder.upsert_merged_docs(temp_col, merged_docs)
+
+    assert len(temp_col.ops) == 1
+    replacement = temp_col.ops[0].replacement
+    assert replacement["_id"] == "MONDO:0005072"
+    assert replacement["mondo"]["mondo"] == "MONDO:0005072"
+    assert replacement["mondo"]["label"] == "neuroblastoma"
+    assert replacement["umls"]["umls"] == "C0700095"
+    assert replacement["original_id"] == "UMLS:C0700095"
